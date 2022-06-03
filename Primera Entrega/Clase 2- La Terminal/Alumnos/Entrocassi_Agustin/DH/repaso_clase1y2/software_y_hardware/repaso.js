e268608f948661103325d51e14a8339febd94a84
unknown <agustinentrocassi@gmail.com>
2022-06-03 18:32:58 -0300

    Entrega Clase 2 a 6, y 8

---
+++ Primera Entrega/Clase 2- La Terminal/Alumnos/Entrocassi_Agustin/DH/repaso_clase1y2/software_y_hardware/repaso.js
@@ -0,0 +1,1 @@
+"El Hardware representa todos los componentes que hacen funcional a una computadora. Cuenta con distintas categorías. El software, por su parte, es todos aquel programa capaz de disponer del hardware de una PC y utilizarlo para funcionar y realizar tareas".
